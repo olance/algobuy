9efd4e2980bd5c50da0b30d6cd3e05ea74318cba
Olivier Lance <olivier@digitalcuisine.fr>
2015-09-05 15:00:31 +0200

    Javascript bundling using Babel (for ES6 transpiling), Browserify (to use module on browser side) and Uglify for production builds

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,6 +1,33 @@
 import gulp from 'gulp';
+import util from 'gulp-util';
+
 import eslint from 'gulp-eslint';
 import scsslint from 'gulp-scss-lint';
+
+import {argv} from 'yargs';
+import browserify from 'browserify';
+import babelify from 'babelify';
+import source from 'vinyl-source-stream';
+import buffer from 'vinyl-buffer';
+import sourcemaps from 'gulp-sourcemaps';
+import gulpif from 'gulp-if';
+import uglify from 'gulp-uglify';
+
+
+const OPTIONS = {
+    browserifyConfig: {
+        entries: 'app/js/algobuy.jsx',
+        basedir: '.',
+        paths: './app/js',
+        extensions: ['js', 'jsx']
+    },
+
+    js: {
+        bundleName: 'algobuy.js',
+        destDir: './dist/js'
+    }
+};
+
 
 // Default task will just run lint
 gulp.task('default', ['lint']);
@@ -21,3 +48,46 @@ gulp.task('scss-lint', () => {
         .pipe(scsslint())
         .pipe(scsslint.failReporter());
 });
+
+
+// BUILDING TASKS
+gulp.task('build', ['js-build', 'scss-build', 'assets-copy']);
+
+gulp.task('js-build', () => {
+    var bundler = browserify(OPTIONS.browserifyConfig);
+
+    bundler.on('error', errorHandler);
+
+    // Use babelify to transpile our ES6 files to ES5
+    bundler.transform(babelify);
+
+    // Bundle our js code
+    bundler.bundle()
+        .pipe(source(OPTIONS.js.bundleName))
+        .pipe(buffer())
+
+        // Generate sourcemaps
+        .pipe(sourcemaps.init({ loadMaps: true }))
+
+        // For production build, run uglify on the bundled js
+        .pipe(gulpif(argv.production, uglify({ mangle: false })))
+        .on('error', errorHandler)
+
+        .pipe(sourcemaps.write('./'))
+        .pipe(gulp.dest(OPTIONS.js.destDir));
+});
+
+gulp.task('scss-build', () => {
+
+});
+
+gulp.task('assets-copy', () => {
+
+});
+
+
+function errorHandler(error)
+{
+    util.log(util.colors.red('!! Error: ' + error.message));
+    this.end();
+}
