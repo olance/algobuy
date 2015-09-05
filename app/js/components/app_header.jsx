6ddd81f2029935f2b094aeffefb0e8752d6b1408
Olivier Lance <olivier@digitalcuisine.fr>
2015-09-05 21:34:38 +0200

    AppHeader component to render the upper part of the page

---
+++ app/js/components/app_header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class AppHeader extends React.Component {
+    render() {
+        return (
+            <header>
+                <img src="images/algobuy.png" alt="AlgoBuy"/>
+            </header>
+        );
+    }
+}
+
+export default AppHeader;
