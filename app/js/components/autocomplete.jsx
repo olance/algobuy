29ea5b854679a2c248fed2527ff04185b789c474
Olivier Lance <olivier@digitalcuisine.fr>
2015-09-05 23:58:44 +0200

    Slightly completed autocomplete markup

--- app/js/components/autocomplete.jsx
+++ app/js/components/autocomplete.jsx
@@ -4,7 +4,11 @@ class Autocomplete extends React.Component {
     render() {
         return (
             <div className="autocomplete">
-                <input type="text"/>
+                <input type="text" placeholder="Type something..."
+                       autofocus="autofocus" autocomplete="off"
+                       spellcheck="false" autocorrect="off"/>
+
+                <div className="input-icon"></div>
             </div>
         );
     }
