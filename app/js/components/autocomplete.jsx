7f2b7b11b53074cfa20c67304d35827cc8d9899d
Olivier Lance <olivier@digitalcuisine.fr>
2015-09-05 21:35:19 +0200

    Autocomplete component, WIP — just showing a text input for the moment

---
+++ app/js/components/autocomplete.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Autocomplete extends React.Component {
+    render() {
+        return (
+            <div className="autocomplete">
+                <input type="text"/>
+            </div>
+        );
+    }
+}
+
+export default Autocomplete;
