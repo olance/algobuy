7bed1969304e8ad66ef7378e4b4531f1d3a2eab2
Olivier Lance <olivier@digitalcuisine.fr>
2015-09-06 23:11:28 +0200

    Requesting both facets we're interested in

--- app/js/algolia/search.js
+++ app/js/algolia/search.js
@@ -4,7 +4,9 @@ import helper from 'algoliasearch-helper';
 const APP_ID = 'PCPLLQEPNP';
 const API_KEY = '47cd5bed96f9000541665a46dba8d031'; // search-only
 const INDEX_NAME = 'AlgoBuyProducts';
-const SEARCH_PARAMS = {};
+const SEARCH_PARAMS = {
+    facets: ['categories', 'price_range']
+};
 
 var client = search(APP_ID, API_KEY);
 
