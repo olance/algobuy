689fe2a9bf7439a7d891631117d9275e70a61dc8
Olivier Lance <olivier@digitalcuisine.fr>
2015-09-07 09:15:34 +0200

    * AutocompleteContainer now listens to the SearchStore too, and renders the SearchSuggestions component * WIP on the SearchSuggestions component: displaying price range tags for the current search

--- app/js/components/autocomplete/autocomplete.jsx
+++ app/js/components/autocomplete/autocomplete.jsx
@@ -3,17 +3,30 @@ import cx from 'classnames';
 
 import React from 'react';
 import {Container} from 'flux/utils';
-import QueryStore from 'stores/query_store.js';
-import SearchInput from './search_input.jsx';
 
+import SearchInput from './search_input.jsx';
+import SearchSuggestions from './search_suggestions.jsx';
+
+import QueryStore from 'stores/query_store.js';
+import SearchStore from 'stores/search_store.js';
+
+// The AutocompleteContainer class is a wrapper around the components that form
+// the autocomplete widget, acting as a controller-view component: it subscribes
+// to the Query- and SearchStore and re-renders itself whenever a change occurs
+// in either of them.
 class AutocompleteContainer extends React.Component {
     static getStores() {
-        return [QueryStore];
+        return [QueryStore, SearchStore];
     }
 
     static calculateState() {
+        var searchResults = SearchStore.getResults();
+
         return {
-            query: QueryStore.getQuery()
+            query: QueryStore.getQuery(),
+            results: searchResults.results || searchResults,
+            params: searchResults.params,
+            error: SearchStore.getLastError()
         }
     }
 
@@ -25,6 +38,7 @@ class AutocompleteContainer extends React.Component {
         return (
             <div className={widgetClasses}>
                 <SearchInput query={this.state.query}/>
+                <SearchSuggestions search={this.state}/>
             </div>
         );
     }
---
+++ app/js/components/autocomplete/search_suggestions.jsx
@@ -0,0 +1,96 @@
+import _ from 'lodash';
+import cx from 'classnames';
+
+import React from 'react';
+
+import * as SearchConstants from 'constants/search_constants.js';
+import SearchActions from 'actions/search_actions';
+import Search from 'algolia/search.js';
+
+class SearchSuggestions extends React.Component {
+    render() {
+        var results = this.props.search.results,
+            emptyQuery = results == SearchConstants.EMPTY_SEARCH_QUERY;
+
+        if(results == SearchConstants.SEARCH_ERROR)
+        {
+            return (
+                <div className="search-error">
+                    Oops! Something wrong happened.
+                </div>
+            );
+        }
+        else if(emptyQuery)
+        {
+            return null;
+        }
+        else if(results.nbHits === 0)
+        {
+            return (
+                <div className="search-suggestions empty-search">
+                    Nothing found :(
+                </div>
+            );
+        }
+        else
+        {
+            // Create a list of all available price range tags for the search
+            // results we received
+            let rangeTags = this._priceRangesList(results);
+
+            return (
+                <div className="search-suggestions">
+                    <div className="price-ranges">
+                        {rangeTags}
+                    </div>
+                </div>
+            );
+        }
+    }
+
+    // Private methods
+    _priceRangesList(results) {
+        // Get all available price_range values
+        var priceRanges = results.getFacetValues('price_range', {
+            sortBy: ['name:asc']
+        });
+
+        // Map all ranges to their tag
+        var rangeTags = priceRanges.map((range) => {
+            return <PriceRangeTag key={range.name} priceRange={range} />;
+        });
+
+        // Add a "any price" tag used when no refinement has been done
+        var anyRange = {
+            name: 'any price',
+            isRefined: !_.any(priceRanges, 'isRefined')
+        };
+
+        rangeTags.unshift(<PriceRangeTag key="any-price"
+                                         priceRange={anyRange}/>);
+
+        return rangeTags;
+    }
+}
+
+export default SearchSuggestions;
+
+// PRIVATE COMPONENTS
+class PriceRangeTag extends React.Component {
+    render() {
+        var className = cx('price-range', {
+            active: this.props.priceRange.isRefined
+        });
+
+        return (
+            <span className={className} onClick={this.clicked.bind(this)}>
+                {this.props.priceRange.name}
+            </span>
+        );
+    }
+
+    clicked() {
+
+    }
+}
+
