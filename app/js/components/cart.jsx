2b096182b8979208151026b2d2a645a94ed6ca9b
Olivier Lance <olivier@digitalcuisine.fr>
2015-09-10 13:50:39 +0200

    Display the list of products added to the cart when its icon is clicked

--- app/js/components/cart.jsx
+++ app/js/components/cart.jsx
@@ -1,9 +1,13 @@
+import _ from 'lodash';
 import cx from 'classnames';
 
 import React from 'react';
 import {Container} from 'flux/utils';
 
+import OutsideClickHandler from 'components/outside_click_handler.jsx';
+
 import CartStore from 'stores/cart_store.js';
+
 
 class CartController extends React.Component {
     static getStores() {
@@ -12,16 +16,30 @@ class CartController extends React.Component {
 
     static calculateState() {
         return {
-            productsCount: CartStore.getProductsCount()
+            productsCount: CartStore.getProductsCount(),
+            products: CartStore.getProducts(),
+            opened: false
         };
     }
 
     render() {
         return (
-            <div className="cart">
-                <CartIcon cart={this.state}/>
-            </div>
+            <OutsideClickHandler onClickOutside={this._outsideClick.bind(this)}>
+                <div className="cart">
+                    <CartIcon cart={this.state} onClick={this._iconClick.bind(this)}/>
+                    <CartProducts cart={this.state}/>
+                </div>
+            </OutsideClickHandler>
         );
+    }
+
+    _iconClick() {
+        var opened = !this.state.opened;
+        this.setState(_.extend({}, this.state, { opened: opened }));
+    }
+
+    _outsideClick() {
+        this.setState(_.extend({}, this.state, { opened: false }));
     }
 }
 
@@ -38,13 +56,45 @@ class CartIcon extends React.Component {
         });
 
         return (
-            <div className="cart-icon">
+            <div className="cart-icon" onClick={this.props.onClick}>
                 <div className={badgeClasses}>
                     <div>
                         {this.props.cart.productsCount}
                     </div>
                 </div>
             </div>
+        );
+    }
+}
+
+class CartProducts extends React.Component {
+    render() {
+        var classes = cx({
+            products: true,
+            hidden: !this.props.cart.opened
+        });
+
+        var products = this.props.cart.products.map((product) => {
+            return <CartProduct key={product.objectID} product={product}/>
+        });
+
+        return (
+            <div className={classes}>
+                <ul>
+                    {products}
+                </ul>
+            </div>
+        );
+    }
+}
+
+class CartProduct extends React.Component {
+    render() {
+        return (
+            <li className="product">
+                <span className="price">${this.props.product.price}</span>
+                <span className="name">{this.props.product.name}</span>
+            </li>
         );
     }
 }
