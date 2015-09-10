f1c3c7f0db76c6ee95ede7e7076d015cd66e95fc
Olivier Lance <olivier@digitalcuisine.fr>
2015-09-10 22:08:38 +0200

    No animated transitions (Masonry is a bit buggy...)

--- app/js/components/results_display.jsx
+++ app/js/components/results_display.jsx
@@ -39,7 +39,7 @@ class ResultsDisplayController extends React.Component {
                 });
 
                 content = (
-                    <Masonry>
+                    <Masonry options={{ transitionDuration: 0}}>
                         {products}
                     </Masonry>
                 );
