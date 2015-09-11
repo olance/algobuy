56a29eb29f22a59191ca04f4fb5a8454018e120d
Olivier Lance <olivier@digitalcuisine.fr>
2015-09-11 12:06:36 +0200

    Fixing invalid constant access

--- app/js/stores/tooltip_store.js
+++ app/js/stores/tooltip_store.js
@@ -1,18 +1,19 @@
 import _ from 'lodash';
 
 import {Store} from 'flux/utils';
+import {Tooltips} from 'constants/tooltip_constants';
 import * as TooltipConstants from 'constants/tooltip_constants';
 import Dispatcher from 'dispatcher/algobuy_dispatcher';
 
 const availableTooltips = {
-    [TooltipConstants.default]: 'Use the arrow keys or [Tab] to navigate this menu',
-    [TooltipConstants.priceRange]: 'Select a price range to refine your search',
-    [TooltipConstants.search]: 'Hit [Enter] to view the results for this search',
-    [TooltipConstants.product]: 'Hit [Enter] to view the details of this product',
-    [TooltipConstants.cart]: 'Hit [Enter] to add this product to your shopping cart'
+    [Tooltips.default]: 'Use the arrow keys or [Tab] to navigate this menu',
+    [Tooltips.priceRange]: 'Select a price range to refine your search',
+    [Tooltips.search]: 'Hit [Enter] to view the results for this search',
+    [Tooltips.product]: 'Hit [Enter] to view the details of this product',
+    [Tooltips.cart]: 'Hit [Enter] to add this product to your shopping cart'
 };
 
-var currentTooltip = TooltipConstants.default;
+var currentTooltip = Tooltips.default;
 
 // The TooltipStore keeps track of the current tooltip displayed at the top of
 // the autocomplete menu
